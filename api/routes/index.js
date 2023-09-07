edcb51394ba6453e26439dc5f39a590126ae968b
flormartinez92 <martinez.mf.92@gmail.com>
2023-09-07 15:29:54 -0300

    create cart routes, incomplete

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,10 +4,11 @@ const router = express.Router();
 const winesAdmRoute = require("./winesAdm");
 const adminUser = require("./AdminUser");
 const user = require("./User");
+const cart = require("./cart");
 
 router.use("/admin", adminUser);
 router.use("/user", user);
 router.use("/wines", winesAdmRoute);
-
+router.use("/cart", cart);
 
 module.exports = router;
